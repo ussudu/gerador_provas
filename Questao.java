4976fad51fde2d1d9741be57cadd712c22c95b9c
PabloYcaro <ycaropablo5@gmail.com>
2026-04-27 22:10:54 -0300

    adicionando getters e setters

--- Questao.java
+++ Questao.java
@@ -6,4 +6,22 @@ public class Questao{
     private Disciplina disciplina;
     private String assunto;
     private int nivelDificuldade;
+
+    //getters
+    public int getCodigo() { return codigo; }
+    public String getTipo() { return tipo; }
+    public String getEnunciado() { return enunciado; }
+    public String getGabarito() { return gabarito; }
+    public Disciplina getDisciplina() { return disciplina; }
+    public String getAssunto() { return assunto; }
+    public int getNivelDificuldade() { return nivelDificuldade; }
+
+    //setters
+    public void setcodigo(int codigo) {this.codigo = codigo;}
+    public void setTipo(String tipo) { this.tipo = tipo; }
+    public void setEnunciado(String enunciado) { this.enunciado = enunciado; }
+    public void setGabarito(String gabarito) { this.gabarito = gabarito; }
+    public void setDisciplina(Disciplina disciplina) { this.disciplina = disciplina; }
+    public void setAssunto(String assunto) { this.assunto = assunto; }
+    public void setNivelDificuldade(int nivelDificuldade) { this.nivelDificuldade = nivelDificuldade; }
 }
